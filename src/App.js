a68f4a6884c79184de1dccfd82098389cad45dc3
Lalitha kuppa <kuppalalitha675@gmail.com>
2025-06-15 16:30:36 +0530

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import User from './components/User';
-import Driver from './components/Driver';
+import Driver from './components/Driver'; 
 import Login from './login';
 import Registration from './Registration';
 import Dashboard from './Dashboard';
@@ -11,36 +11,28 @@ import LoginDashboard from './LoginDashboard';
 import Bookings from './Bookings';
 import ResetPassword from './ResetPassword';
 import AdminDashboard from './AdminDashboard';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 import { ThemeProvider } from './context/ThemeContext';
 
 function App() {
-  return (
-    <ThemeProvider>
-      <Router>
-        <Routes>
-          <Route path="/user" element={<User />} />
-          <Route path="/driver" element={<Driver />} />
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/reset-password" element={<ResetPassword />} />
-          <Route path="/location" element={<Location />} />
-          <Route path="/bookings" element={<Bookings />} />
-          <Route path="/admin" element={<AdminDashboard />} />
-          <Route path="/register" element={<Registration />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/logindashboard" element={<LoginDashboard />} />
-        </Routes>
-        <ToastContainer
-          position="top-right"
-          autoClose={3000}
-          pauseOnHover
-          draggable
-        />
-      </Router>
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider>
+            <Router>
+                <Routes>
+                    <Route path="/user" element={<User />} />
+                    <Route path="/driver" element={<Driver />} />
+                    <Route path="/" element={<Dashboard />} />  
+                    <Route path="/reset-password" element={<ResetPassword />} />
+                    <Route path="/location" element={<Location />} />  
+                    <Route path="/bookings" element={<Bookings />} />  
+                    <Route path="/admin" element={<AdminDashboard />} />
+                    <Route path="/register" element={<Registration />} />  
+                    <Route path="/login" element={<Login />} /> 
+                    <Route path="/forgot-password" element={<ForgotPassword />} />
+                    <Route path="/logindashboard" element={<LoginDashboard />} /> 
+                </Routes>
+            </Router>
+        </ThemeProvider>
+    );
 }
 
 export default App;
